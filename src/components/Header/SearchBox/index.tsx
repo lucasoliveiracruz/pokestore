b28a4a0c74c44451f016b99d75ca63ca89942b0e
Lucas Oliveira <lucas.oliveira.timburi@gmail.com>
2021-08-28 13:41:33 -0300

    change search icon color to primary

--- src/components/Header/SearchBox/index.tsx
+++ src/components/Header/SearchBox/index.tsx
@@ -1,14 +1,17 @@
 import { RiSearchLine } from "react-icons/ri";
+import { useTheme } from "styled-components";
 import { useStore } from "../../../contexts/StoreContext";
 
 import { SearchContainer, SearchInput } from "./styles";
 
 export function SearchBox() {
   const { store } = useStore();
+  const theme = useTheme();
+
   return (
     <SearchContainer>
       <SearchInput placeholder={store.translations.header.search_placeholder} />
-      <RiSearchLine size={20} />
+      <RiSearchLine size={24} color={theme.color.primary} />
     </SearchContainer>
   );
 }
