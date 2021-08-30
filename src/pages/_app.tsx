8350338de1d723f2cc390dbad1142b846a9d56e9
Lucas Oliveira <lucas.oliveira.timburi@gmail.com>
2021-08-29 22:31:37 -0300

    fix: get theme from env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,11 @@ import { StoreProvider } from "../contexts/StoreContext";
 import { PokeStores } from "../models/PokeStores";
 import GlobalStyles from "../styles/global";
 
+const STORE = process.env.NEXT_PUBLIC_POKESTORE_THEME as PokeStores;
+
 export default function MyApp({ Component, pageProps }) {
   return (
-    <StoreProvider store={PokeStores.GRASS}>
+    <StoreProvider store={STORE}>
       <CartProvider>
         <Component {...pageProps} />
         <GlobalStyles />
