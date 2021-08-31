8899fb5337966f01f435bbab58e4533715f0cc16
Lucas Oliveira <lucas.oliveira.timburi@gmail.com>
2021-08-31 20:09:45 -0300

    refactor store

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,15 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-// import { Button } from "../components/Button";
-import { Cart } from "../components/Cart";
 import { Header } from "../components/Header";
+import { LoadingCover } from "../components/LoadingCover";
 import { ProductsList } from "../components/ProductsList";
-import { useCart } from "../contexts/CartContext";
 import { useStore } from "../contexts/StoreContext";
-import { PokeStores } from "../models/PokeStores";
-import { ProductsService } from "../services";
-import { PokemonProduct } from "../services/types";
+import { useProductsList } from "../services/hooks/useProductsList";
 import { Container } from "../styles/Store.styles";
 import { capitalize } from "../utils/capitalize";
 
-// function randomTheme() {
-//   const validStores = [PokeStores.FIRE, PokeStores.GRASS, PokeStores.WATER];
-//   const random = validStores[Math.floor(Math.random() * validStores.length)];
-//   return random;
-// }
-
 export default function Store() {
-  const { currentStore, setStore } = useStore();
-  const { addToCart } = useCart();
-
-  const [products, setProducts] = useState<PokemonProduct[]>([]);
-  const [isCartVisible, setIsCartVisible] = useState(false);
-
-  useEffect(() => {
-    ProductsService.getAllPokemonProductsByType(currentStore).then(
-      (response) => {
-        setProducts(response);
-      }
-    );
-  }, [currentStore]);
-
-  function toggleCartVisible() {
-    setIsCartVisible((visible) => !visible);
-  }
+  const { currentStore } = useStore();
+  const { products, isLoadingProducts } = useProductsList();
 
   return (
     <>
@@ -43,26 +17,10 @@ export default function Store() {
         <title>{capitalize(currentStore)} | PokeStore</title>
       </Head>
       <Container>
-        <Header onCartClick={toggleCartVisible} />
-
-        {/* <Button onClick={() => setStore(randomTheme())}>Trocar de loja</Button> */}
-        <ProductsList
-          products={products}
-          onClickItem={(item) =>
-            addToCart({
-              id: String(item.id),
-              image_url: item.image_url,
-              price: item.price,
-              title: item.name,
-            })
-          }
-        />
+        <Header />
+        <LoadingCover isVisible={isLoadingProducts} />
+        <ProductsList products={products} />
       </Container>
-
-      <Cart
-        isCartVisible={isCartVisible}
-        toggleCartVisible={toggleCartVisible}
-      />
     </>
   );
 }
